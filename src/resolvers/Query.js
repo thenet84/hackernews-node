07a2acfbee246e7e202980f5cc97bef87182e2d2
diglesib <the.net.wo@gmail.com>
2018-08-25 03:59:58 +0100

    Filtering, Pagination and Sorting

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,7 +3,16 @@ function info(){
 }
 
 function feed(root, args, context, info){
-  return context.db.query.links({}, info);
+  const where = args.filter 
+    ? {
+      OR: [
+        {url_contains: args.filter},
+        {description_contains: args.filter}
+      ]
+    } : {};
+  return context.db.query.links({ 
+    where, skip: args.skip, first: args.first, orderBy: args.orderBy
+  }, info);
 }
 
 function link(root, args, context, info){
